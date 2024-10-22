671ef2cff0125d9a0e8eb0c1022dce9c0e36b447
wacharat thongsomboon <wacharatthongsomboon@wacharats-MacBook-Pro.local>
2024-10-22 11:39:58 +0700

    Restructure for model package

--- app/controller/users/login.go
+++ app/controller/users/login.go
@@ -1,15 +1,20 @@
 package users
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"regexp"
 
-	"github.com/JackleStyle0/micro_services_practice/app/models"
+	models "github.com/JackleStyle0/micro_services_practice/app/models/user"
 	"github.com/gin-gonic/gin"
 )
 
 func Login(c *gin.Context) {
-	var login models.UserLogin
+	mockEmail := "yao@odds.team"
+	mockPassword := "382e0360e4eb7b70034fbaa69bec5786"
+
+	var login models.Login
 	err := c.BindJSON(&login)
 	if err != nil {
 		fmt.Println(err)
@@ -20,8 +25,15 @@ func Login(c *gin.Context) {
 	}
 
 	isValid := validateEmailPatten(login.Email)
-	if isValid {
-		c.JSON(200, "token")
+	if !isValid {
+		c.JSON(400, "invalid request")
+	}
+
+	hash := md5.Sum([]byte(login.Password))
+	hashPassword := hex.EncodeToString(hash[:])
+
+	if login.Email == mockEmail && hashPassword == mockPassword {
+		c.JSON(200, "OK")
 	} else {
 		c.JSON(400, "invalid request")
 	}
