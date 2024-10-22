939be454dc463d8d51fda5af037cab812e19bf2d
wacharat thongsomboon <wacharatthongsomboon@wacharats-MacBook-Pro.local>
2024-10-22 09:01:16 +0700

    Create routes to config all routes in this package

---
+++ config/routes.go
@@ -0,0 +1,11 @@
+package config
+
+import (
+	"github.com/JackleStyle0/micro_services_practice/app/controller/users"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) *gin.Engine {
+	r.POST("/users/login", users.Login)
+	return r
+}
