21b31284257121522ded52c5e6d1430adb4b7a58
wacharat thongsomboon <wacharatthongsomboon@wacharats-MacBook-Pro.local>
2024-10-24 10:02:29 +0700

    move connect db to main.go

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/JackleStyle0/micro_services_practice/config"
-	"github.com/gin-gonic/gin"
+	db "github.com/JackleStyle0/micro_services_practice/db/sqlc"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+const (
+	dbSource = "postgres://root:root@localhost:5432/auth_development?sslmode=disable"
+)
+
+var queries *db.Queries
+
 func main() {
-	r := gin.Default()
-	r = config.SetupRoutes(r)
+
+	conPool, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to database")
+	}
+
+	queries = db.New(conPool)
+	r := config.SetupRoutes()
 	r.Run("localhost:3000")
 }
